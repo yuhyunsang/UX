40a4b9173e352ebb6cabd9bd94dd4be543042ec2
yuhyunsang <gustkddld@gmail.com>
2021-07-21 10:09:32 +0900

    first commit

---
+++ IAD_01_MakeCircle/app.js
@@ -0,0 +1,45 @@
+class App {
+    constructor () {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.PixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+        resize() {
+            this.stageWidth = document.body.clientWidth;
+            this.stageHeight = document.body.clientHeight;
+
+            this.canvas.width = this.stageWidth * this.PixelRatio;
+            this.canvas.height = this.stageHeight * this.PixelRatio;
+            this.ctx.scale(this.PixelRatio, this.PixelRatio);
+        }
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight)
+
+        this.ctx.fillStyle = '#cddb49';
+        this.ctx.beginPath();
+        this.ctx.arc(
+            this.stageWidth / 2,
+            this.stageHeight / 2,
+            300,
+            0, 2 * Math.PI
+        );
+        this.ctx.fill();
+    }
+
+
+}
+
+
+window.onload = () => {
+    new App();
+}
